d1df10d5e7fe5ae2e32cba41b4d37ed28b9965f6
Juskoh Afonso <juskoh_96@hotmail.com>
2023-03-10 21:16:39 +0000

    Article Arena concluido, imagens e SetInterval foram adicionado, agora vamos para o Responsive >:D

---
+++ java/arena.js
@@ -0,0 +1,7 @@
+let angulo = 0;
+let cartas = document.querySelector(".article-3-box-cards");
+function rodar() {
+  angulo -= 60;
+  cartas.style.transform = `translateZ(-25rem) rotateY(${angulo}deg)`;
+}
+setInterval(rodar, 6000);
